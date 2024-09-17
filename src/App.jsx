78292f17b36364686d754a074d5200b38fa055f6
arjit88 <arjitanand88@gmail.com>
2024-09-17 19:09:51 +0530

    Install react-icons and react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import AuthPage from "./pages/AuthPage/AuthPage";
 
 function App() {
-  return <>Insta CLone</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
