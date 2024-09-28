7cea10ae4bfb1a6fe50db464f7a1e52c2eb74ea0
arjit88 <arjitanand88@gmail.com>
2024-09-28 21:55:46 +0530

    Social-app

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -39,7 +39,10 @@ const FeedPosts = () => {
           <Text fontSize={"md"} color={"red.400"}>
             Dayuum. Looks like you don&apos;t have any friends.
           </Text>
-          <Text color={"red.400"}>Stop coding and go make some!!</Text>
+          <Text color={"red.400"}>
+            Stop coding and go make some!! Follow from right side suggested
+            users.
+          </Text>
         </>
       )}
     </Container>
